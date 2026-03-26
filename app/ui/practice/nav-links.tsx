88186535fb200da3b16e9ac9d984f76a6bb71c07
Gary Xia (myopicOracle) <184602237+myopicOracle@users.noreply.github.com>
2026-03-26 14:10:14 +0700

    Complete nav-links component; move practice page dash to dynamic route

--- app/ui/practice/nav-links.tsx
+++ app/ui/practice/nav-links.tsx
@@ -1,3 +1,37 @@
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const links = [
+    {
+        name: 'Progress',
+        href: '/practice',
+    },
+    {
+        name: 'Exams',
+        href: '/practice/exams',
+    },
+    {
+        name: 'Flashcards',
+        href: '/practice/flashcards',
+    },
+]
+
 export default function NavLinks() {
-    return <div></div>
+    const pathname = usePathname()
+    return (
+        <>
+            {links.map((link) => {
+                return (
+                    <Link
+                        key={link.name}
+                        href={link.href}
+                        className={`flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3 ${
+                            pathname === link.href
+                                ? 'bg-sky-100 text-blue-600'
+                                : 'bg-gray-50'
+                        }`}></Link>
+                )
+            })}
+        </>
+    )
 }
