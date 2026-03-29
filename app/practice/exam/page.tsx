570f4ef26b18961d58e69743735dc60ba5db8f45
Gary Xia (myopicOracle) <184602237+myopicOracle@users.noreply.github.com>
2026-03-29 18:55:39 +0700

    Refactor exam page component to use server-side searchParams

--- app/practice/exam/page.tsx
+++ app/practice/exam/page.tsx
@@ -3,13 +3,25 @@ import Card from '@/app/ui/practice/exam/card'
 import ProgressBar from '@/app/ui/practice/exam/progress-bar'
 import { fetchQuestions } from '@/app/lib/data'
 
-export default async function Page() {
-    const examID = 'DEA-C01 - Exam 1' // NTD: make dynamic (pull from questions table)
+export default async function Page({
+    searchParams,
+}: {
+    searchParams: Promise<{
+        id?: string
+    }>
+}) {
+    const params = await searchParams
 
     const questions = await fetchQuestions()
-    console.log('Object returned: ', questions)
+    console.log('Questions table returned: ', questions)
 
-    const currQuestion = questions[0] // NTD: replace with URL Params, hardcoded for now
+    const currentID = Number(params?.id) || 1
+    console.log('currentID: ', currentID)
+    const currentQuestion = questions[currentID]
+    console.log('currentQuestion: ', currentQuestion)
+    const totalQuestions = questions.length
+
+    const examID = 'DEA-C01 - Exam 1' // NTD: make dynamic (pull from questions table)
 
     return (
         <div className="w-full">
@@ -18,7 +30,7 @@ export default async function Page() {
             <ProgressBar />
 
             {/* NTD: dynamically generate with map, index with URL params */}
-            <Card id={0} question={currQuestion} />
+            <Card id={currentID} question={currentQuestion} />
         </div>
     )
 }
