b9f388b4b0ecd4fca56ddbf4c3d8a86d2cef235d
Gary Xia (myopicOracle) <184602237+myopicOracle@users.noreply.github.com>
2026-04-15 12:16:57 +0800

    Implement file saving for extracted markdown, assess quality of conversion

--- scripts/extract_docs.py
+++ scripts/extract_docs.py
@@ -1,37 +1,27 @@
 from docling.document_converter import DocumentConverter
 
-file_path = "data/raw_pdfs/AWS-Certified-Machine-Learning-Engineer-Associate_Exam-Guide.pdf"
+pdf_path = "docs/sample-pdfs/AWS-Certified-Machine-Learning-Engineer-Associate_Exam-Guide.pdf"
+
+pdf_file_name = pdf_path.split("/")[-1]
+markdown_file_name = pdf_file_name.replace(".pdf", ".md")
+
+markdown_prefix = "data/markdown/"
+markdown_path = markdown_prefix + markdown_file_name
+# print(markdown_path)
 
 print("Spinning up PDF extraction engine")
 converter = DocumentConverter()
 
 print("Beginning extraction process")
-raw_text = converter.convert(file_path)
-
+raw_text = converter.convert(pdf_path)
 markdown_text = raw_text.document.export_to_markdown()
 
-print("Extraction completed. Previewing first 500 chars:")
-print(markdown_text[:500])
+# print("Extraction completed. Previewing first 500 chars:")
+# print(markdown_text[:500])
 
+print(f"Saving to {markdown_path}")
+with open(markdown_path, "w", encoding="utf-8") as new_file:
+    new_file.write(markdown_text)
 
-# npm run docs:extract
-
-# > docs-app@0.1.0 docs:extract
-# > .venv/bin/python scripts/extract_docs.py
-
-# Spinning up PDF extraction engine
-# Beginning extraction process
-# Loading weights: 100%|██████████████████████████████████████████████████████████████████████████████████████████| 770/770 [00:00<00:00, 2564.25it/s]
-# Extraction completed. Previewing first 500 chars:
-# <!-- image -->
-
-# ## AWS Certified Machine Learning Engineer -Associate (MLA-C01) Exam Guide
-
-# ## Introduction
-
-# The AWS Certified Machine Learning Engineer - Associate (MLA-C01) exam validates a candidate's ability to build, operationalize, deploy, and maintain machine learning (ML) solutions and pipelines by using the AWS Cloud.
-
-# The exam also validates a candidate's ability to complete the following tasks:
-
-# -  Ingest, transform, validate, and prepare data for ML modeling.
-# -  Select general mode
+print("Markdown saved successfully")
+print("- Extraction Engine Stopped -")
