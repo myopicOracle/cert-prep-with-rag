d814da8eebe8d496bcebfca7029701a9f622cd03
Gary Xia (myopicOracle) <184602237+myopicOracle@users.noreply.github.com>
2026-04-17 12:28:57 +0800

    Config Supabase client, split embed/load stages, and test single db insert

--- scripts/load.py
+++ scripts/load.py
@@ -1,68 +1,31 @@
 import json
 import os
 
-import boto3
 from dotenv import load_dotenv
+from supabase import create_client
 
 load_dotenv(".env.local")
 
-bedrock = boto3.client(
-    "bedrock-runtime",
-    region_name=os.getenv("AWS_REGION"),
+supabase = create_client(
+    os.getenv("NEXT_PUBLIC_SUPABASE_URL"),
+    os.getenv("SUPABASE_SERVICE_ROLE_KEY"),
 )
 
-input_path = "data/without-embeddings.json"
-output_path = "data/with-embeddings.json"
+input_path = "data/with-embeddings.json"
 
+print("Loading documents...\n")
+with open(input_path, "r", encoding="utf-8") as my_file:
+    documents = json.load(my_file)
+    print("Load success.")
+    print(f"Number of documents: {len(documents)}")
+    print(f"Embedding size: {len(documents[0]['embedding'])}\n")
 
-def get_embedding(raw_text):
-    response = bedrock.invoke_model(
-        modelId="amazon.titan-embed-text-v2:0",
-        contentType="application/json",
-        accept="application/json",
-        body=json.dumps(
-            {
-                "inputText": raw_text,
-                "dimensions": 1024,
-                "normalize": True,
-            }
-        ),
-    )
+single_row = {
+    "content": documents[0]["content"],
+    "metadata": documents[0]["metadata"],
+    "source_url": documents[0]["source_url"],
+    "embedding": documents[0]["embedding"],
+}
 
-    body = json.loads(response["body"].read())
-    # print(f"Embedding length: {len(body['embedding'])}")
-
-    return body["embedding"]
-
-
-def main():
-    with open(input_path, "r", encoding="utf-8") as my_file:
-        document_chunks = json.load(my_file)
-
-    counter = 0
-    for chunk in document_chunks:
-        embedding = get_embedding(chunk["content"])
-        chunk["embedding"] = embedding
-
-        counter += 1
-        if counter % 5 == 0 or counter == len(document_chunks):
-            print(f"Success. Added embeddings to {counter} documents.")
-        else:
-            progress = counter % 5
-            print("." * progress)
-
-    # save documents with embeddings added
-    with open(output_path, "w", encoding="utf-8") as f:
-        json.dump(document_chunks, f, indent=2)
-
-    print("\nSaved data/with-embeddings.json")
-
-
-if __name__ == "__main__":
-    main()
-
-    # Type - (Raw Chunk No Embedding): <class 'dict'> | has keys: 'content', 'metadata', 'source_url'
-    # Type - (Returned Embedding Obj): <class 'dict'> | has keys: 'embedding', 'inputTextTokenCount'
-    # Type - (embedding['embedding']): <class 'list'> | has items: type float
-    # Type - (embedding['embedding'][0]): <class 'float'>
-    # Type - (Chunk WITH Embedding): <class 'dict'> | has keys: 'content', 'metadata', 'source_url', 'embedding'
+result = supabase.table("documents").insert(single_row).execute()
+print(f"Result of Supabase inser: {result}\n")
