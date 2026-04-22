a1e66c49e00e1a528835505653a3c07dc1ec967a
Gary Xia (myopicOracle) <184602237+myopicOracle@users.noreply.github.com>
2026-04-22 17:46:04 +0800

    Add state and fetch fn for llm response, wire up ChatInput to page

--- app/practice/study/page.tsx
+++ app/practice/study/page.tsx
@@ -1,15 +1,24 @@
 'use client'
 
+import { useState } from 'react'
 import { lusitana } from '@/app/ui/fonts'
 import ChatInput from '@/app/ui/practice/study/chat-input'
 
 export default function Page() {
+    const [assistantResponse, setAssistantResponse] = useState<string | null>(null)
+
+    function fetchResponse(query: string) {
+        console.log('Page Component - query: ')
+        setAssistantResponse(query)
+    }
+
     return (
         <div className="w-full">
             <h1 className={`${lusitana.className} text-2xl`}>Study Mode</h1>
             <p className="mt-2 text-gray-500">Get answers from real AWS documentation.</p>
             <div className="mt-4">
-                <ChatInput />
+                {assistantResponse}
+                <ChatInput onSubmit={fetchResponse} />
             </div>
         </div>
     )
