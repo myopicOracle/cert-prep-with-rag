28abe72674e324f019331a4abbb68c94a2c346b2
Gary Xia (myopicOracle) <184602237+myopicOracle@users.noreply.github.com>
2026-04-22 23:57:54 +0800

    Add role-based styling and conditional rendering, closes #35

--- app/practice/study/page.tsx
+++ app/practice/study/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react'
 import { lusitana } from '@/app/ui/fonts'
-import ReactMarkdown from 'react-markdown'
 import ChatInput from '@/app/ui/practice/study/chat-input'
 import ChatMessage from '@/app/ui/practice/study/chat-message'
 
@@ -36,7 +35,9 @@ export default function Page() {
             <h1 className={`${lusitana.className} text-2xl`}>Study Mode</h1>
             <p className="mt-2 text-gray-500">Get answers from real AWS documentation.</p>
             <div className="mt-4">
-                <ChatMessage role={'assistant'} content={assistantResponse} />
+                {assistantResponse && (
+                    <ChatMessage role={'assistant'} content={assistantResponse} />
+                )}
                 <ChatInput onSubmit={fetchResponse} />
             </div>
         </div>
--- app/ui/practice/study/chat-message.tsx
+++ app/ui/practice/study/chat-message.tsx
@@ -3,10 +3,14 @@ import { ChatMessageProps } from '@/app/types/components'
 
 export default function ChatMessage({ role, content }: ChatMessageProps) {
     return (
-        <div>
-            {content && (
-                <div className="prose prose-sm mb-4">
-                    <ReactMarkdown>{`*${role}*: ${content}`}</ReactMarkdown>
+        <div className="flex mb-4">
+            {role === 'user' ? (
+                <div className="max-w-[66%] rounded-lg bg-blue-300 prose prose-sm">
+                    <ReactMarkdown>{content}</ReactMarkdown>
+                </div>
+            ) : (
+                <div className="mr-auto max-w-[66%] rounded-lg bg-gray-300 prose prose-sm">
+                    <ReactMarkdown>{content}</ReactMarkdown>
                 </div>
             )}
         </div>
